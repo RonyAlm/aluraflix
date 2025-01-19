11b7e3def3d151b3de49b861c3f02a09667bc000
Ronaldo Almirón <rony.almiron.015@gmail.com>
2025-01-18 22:19:10 -0300

    Corregir la importación del componente Home en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Home from './pages/home'
+import Home from './pages/Home'
 import NewVideo from './pages/NewVideo'
 
 function App() {
